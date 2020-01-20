71e322c993917f1e0849d363ae3cc7ea1ed0d3ef
hagier <hagierek@gmail.com>
2020-01-20 15:41:34 +0100

    Initial commit

---
+++ client.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+ 
+"""A short description of the module -- called a docstring."""
+ 
+# Here comes your imports
+import traceback
+import socketserver
+import signal
+import socket
+import threading
+import sys
+import base64
+import requests
+import uuid
+import time
+ 
+# Here comes your (few) global variables
+BIND_ADDRESS = '127.0.0.1'
+BIND_PORT = 9999
+
+SERVER_ADDRESS = '127.0.0.1'
+SERVER_PORT = 8080
+
+CLIENT_TIMEOUT = 60
+ 
+# Here comes your class definitions
+class ClientConnectionThread(threading.Thread):
+    def __init__(self, client_socket, client_address):
+        super(ClientConnectionThread, self).__init__()
+        self.daemon = True
+        self.flag_stop = False
+        self.client_socket = client_socket
+        self.client_address = client_address
+        self.session_id = str(uuid.uuid1())
+        #print("Initiated ClientConnectionThread.")
+         
+    def run(self):
+        print(f"Starting HTTP proxy between {self.client_address} and {SERVER_ADDRESS}:{SERVER_PORT}.")
+        
+        proxies = {
+             "http": None,
+             #"http": "http://localhost:8555",
+             "https": None,
+            }
+            
+        client_timeout = 0
+        client_timestamp = time.time()
+        
+        while not self.flag_stop:
+            try:   
+                #time.sleep(1)
+                received_from_local = self.socket_receive(self.client_socket)
+                
+                current_timestamp = time.time()
+                proxy_response = None
+                if not received_from_local:
+                    if (current_timestamp-client_timestamp)/1.0 > client_timeout/CLIENT_TIMEOUT:
+                        proxy_response = requests.post(f"http://{SERVER_ADDRESS}:{SERVER_PORT}/", data=b'', headers={str("Session-Id"): self.session_id}, proxies = proxies)
+                    if (current_timestamp-client_timestamp) > 1.0:
+                        client_timeout += 1
+                        client_timestamp = current_timestamp
+                        print(f"Update timestamp {client_timestamp}, count {client_timeout}.")
+                    else:
+                        continue
+                else:
+                    proxy_response = requests.post(f"http://{SERVER_ADDRESS}:{SERVER_PORT}/", data=received_from_local, headers={str("Session-Id"): self.session_id}, proxies = proxies)
+                    client_timeout = 0
+                    client_timestamp = current_timestamp
+                  
+                # Disconnect client who passed CLIENT_TIMEOUT variable value
+                if client_timeout > CLIENT_TIMEOUT:
+                    print(f"Client {self.client_address} disconnected. Timeout {CLIENT_TIMEOUT} seconds.")
+                    break
+                
+                received_messages = None
+                if proxy_response:
+                    received_messages = str(proxy_response.content, "utf8")
+                
+                if received_messages and received_messages != b'':
+                    print(f"Received message packet from PROXY: {received_messages}")
+                    for message in received_messages.split(';'):
+                        print(f"Message from packet: {message}")
+                        print("DECODED:", str(base64.b64decode(message)))
+                        self.socket_send(self.client_socket, message)
+                
+                
+                time.sleep(0.1)
+                         
+            except socket.timeout:
+                print(f"Client disconnected.")
+                break
+            except requests.exceptions.ConnectionError:
+                print(f"\n[ERROR] Could not connect to server {SERVER_ADDRESS}:{SERVER_PORT}\n")
+                break
+            except:
+                traceback.print_exc()
+                break
+                    
+                   
+        self.client_socket.close()
+        print('Stopping thread.')
+        sys.exit(0)
+        
+    def socket_receive(self, socket_local):
+        chunks = []
+        bytes_recd = 0
+        chunk = b''
+        while b'\n' not in chunk:
+            try:
+                socket_local.settimeout(0.0)
+                chunk = socket_local.recv(65000)
+                if chunk == b'':
+                    break
+                chunks.append(chunk)
+                bytes_recd = bytes_recd + len(chunk)
+            except BlockingIOError:
+                break
+            except socket.timeout:
+                pass
+            except:
+                traceback.print_exc()
+                break
+        
+        data = b''.join(chunks)
+        if data != b'':
+            print(f"Received from local: {data}")
+            data_encoded = base64.b64encode(data)
+            return data_encoded
+        return None
+        
+    def socket_send(self, socket, message):
+        #print('Forwarding to ' + str(socket.getpeername()) + ' message: ' + str(message))
+        if message != b'':
+            message_decoded = base64.b64decode(message)
+            print('\nForwarding to ' + str(socket.getpeername()) + ' message: ' + str(message_decoded))
+            socket.settimeout(5)
+            socket.sendall(message_decoded)
+ 
+class HTTProxerClient:
+
+    def __init__(self):
+        # Shutdown on Ctrl+C
+        signal.signal(signal.SIGINT, self.shutdown) 
+        
+        # Create a TCP socket
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Re-use the socket
+        self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # Bind the socket to a public host, and a port   
+        print(f'Starting listener on {BIND_ADDRESS}:{BIND_PORT}....')
+        self.serverSocket.bind((BIND_ADDRESS, BIND_PORT))
+        self.serverSocket.listen(10) # become a server socket
+        self.__clients = {}
+        
+        print("Listerner started. Awaiting connections...")
+        
+        while True:
+            try:
+                # Establish the connection
+                self.serverSocket.settimeout(1)
+                (client_socket, client_address) = self.serverSocket.accept() 
+                
+                print(f"Starting thread for {client_address}:{client_socket}.")
+
+                #d = threading.Thread(name=self._getClientName(client_address), 
+                connection_thread = ClientConnectionThread(client_socket = client_socket, client_address = client_address)
+                connection_thread.start()
+                print("Started")
+            except socket.timeout:
+                #print("Timeout")
+                pass
+            except e:
+                traceback.print_exc()
+                break
+        
+    def shutdown(self, signum, frame):
+        """ Handle the exiting server. Clean all traces """
+        print("Shutting down gracefully...")
+        self.serverSocket.close()
+        print("Próbuję wyjść...")
+        sys.exit(0)
+        
+# Here comes your function definitions
+
+def main():
+    """Launcher."""
+    print("Witaj w kliencie HTTProxer.")
+    # init the GUI or anything else
+    proxy = HTTProxerClient()
+    pass
+ 
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+ 
+"""A short description of the module -- called a docstring."""
+ 
+# Here comes your imports
+import http.server
+import socketserver
+import signal
+import sys
+import threading
+import socket
+import base64
+import time
+import uuid
+import traceback
+
+# Here comes your (few) global variables
+BIND_ADDRESS = '0.0.0.0'
+BIND_PORT = 8080
+
+REMOTE_ADDRESS = '10.6.7.66'
+REMOTE_PORT = 22
+
+class ServerConnectionThread(threading.Thread):
+    #incoming_message = []
+    outgoing_messages = []
+
+    def __init__(self, name):
+        super(ServerConnectionThread, self).__init__()
+        self.daemon = True
+        self.name = name
+        self.flag_stop = False
+        self.socket_remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print(f"Connecting thread {self.name} to remote port {REMOTE_ADDRESS}:{REMOTE_PORT}...")
+        self.socket_remote.connect((REMOTE_ADDRESS, REMOTE_PORT))
+        print(f"Connected to {REMOTE_ADDRESS}:{REMOTE_PORT}.")
+        
+    def run(self):
+        while not self.flag_stop:
+            data_received = self._socket_receive(self.socket_remote)
+            if data_received != b'':
+                print(f"\nAdding message from socket [{data_received}] to outgoing messages.")
+                self.outgoing_messages.append(str(base64.b64encode(data_received), "utf8"))
+                continue
+            time.sleep(0.1)            
+        
+        print(f"End of thread {self.name}.")
+        
+    def stop(self):
+        self.flag_stop = True
+        self.socket_remote.close()
+        
+    def send_message(self, message):
+        data = base64.b64decode(message)
+        self._socket_send(self.socket_remote, data)
+        
+    def receive_messages(self):
+        if len(self.outgoing_messages) > 1:
+            print("Joining outgoing messages...")
+            messages = ';'.join(self.outgoing_messages)
+        elif len(self.outgoing_messages) == 1:
+            print("Joining 111111111 outgoing messages...")
+            messages = self.outgoing_messages[0]
+        else:
+            messages = ""
+        self.outgoing_messages = []        
+        return messages
+        
+        
+    def _socket_receive(self, socket_remote):
+        chunks = []
+        bytes_recd = 0
+        chunk = b''
+        
+        while b'\n' not in chunk:
+            try:
+                socket_remote.settimeout(0.0)
+                chunk = socket_remote.recv(65000)
+                if chunk == b'':
+                    break
+                chunks.append(chunk)
+                bytes_recd = bytes_recd + len(chunk)
+            except BlockingIOError:
+                break
+            except socket.timeout:
+                pass
+            except:
+                traceback.print_exc()
+                break
+        return b''.join(chunks)
+        
+    def _socket_send(self, socket_remote, message):
+        #if message != b'KEEP_ALIVE':
+        socket_remote.sendall(message)
+        
+ 
+# Here comes your class definitions
+class HTTProxerRequestHandler(http.server.BaseHTTPRequestHandler):
+    def _set_headers(self, code = 200):
+        self.send_response(code)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+    
+    def version_string(self):
+        return "Unknown"
+
+    def _html(self, message):
+        """This just generates an HTML document that includes `message`
+        in the body. Override, or re-write this do do more interesting stuff.
+        """
+        content = f"{message}"
+        return content.encode("utf8")  # NOTE: must return a bytes object!
+        
+    def send_head():
+        """Empty"""
+        
+    def set_socket_remote(self, socket_to_assign):
+        self.socket_remote = socket_to_assign
+        
+    def response_fake(self):
+        self._set_headers(500)
+        self.wfile.write(self._html("Service unavailable. Thread: " + threading.currentThread().getName() + "\n"))
+
+    def do_GET(self):
+        self.response_fake()
+
+    def do_HEAD(self):
+        self.response_fake()
+
+    def do_POST(self):               
+        #time.sleep(1)
+        if self.headers["Session-Id"] is None:
+            self.response_fake()
+            return
+            
+        request_data = self.rfile.read(int(self.headers.get('content-length')))
+            
+        request_data_decoded = base64.b64decode(request_data)
+        print(f"\nRequest data: {request_data}. Decoded: {request_data_decoded}.")
+        
+        connection_thread = self.find_thread(self.headers["Session-Id"])
+        connection_thread.send_message(request_data)
+            
+        #print("Showing threads before find:", threading.enumerate())
+        #print("Result of thread search: ", self.find_thread(self.headers["Session-Id"]))
+        #print("Showing threads after find:", threading.enumerate())
+        #print("Client session ID: " + str(self.headers["Session-Id"]))
+        
+        try:
+            # Send HTTP response
+            self._set_headers()
+            messages_to_send = connection_thread.receive_messages()
+            print(f"Sending full response: {messages_to_send}")
+            self.wfile.write(self._html(messages_to_send))
+        except:
+            traceback.print_exc()
+            pass    
+        
+    # Returns thread if found (based on name) or None if there's no .
+    def find_thread(self, thread_name):
+        # Look for thread with name equal to thread_name
+        for thread in threading.enumerate():
+            if thread.name == thread_name:
+                print(f"Found thread named {thread_name}! Not starting new thread")
+                return thread
+        # If not found, create a new thread
+        print(f"Thread named {thread_name} not found. Starting new thread")
+        thread_new = ServerConnectionThread(thread_name)
+        thread_new.start()
+        return thread_new
+        
+class HTTProxerServer:
+    def __init__(self):
+        # Shutdown on Ctrl+C
+        signal.signal(signal.SIGINT, self.shutdown)
+                                
+        self.start_http1_server()
+        
+    def start_http1_server(self):
+        print("Initializing HTTP1 server.")
+        http1_handler = HTTProxerRequestHandler
+
+        with http.server.ThreadingHTTPServer((BIND_ADDRESS, BIND_PORT), http1_handler) as httpd:
+            print(f"Server started at {BIND_ADDRESS}:{BIND_PORT}.")
+            httpd.serve_forever()
+            
+    def start_http2_server(self):
+        print(f"Initializing HTTP2 server on {BIND_ADDRESS}:{BIND_PORT}.")
+        
+    def thread_test(self):
+        while True:
+            """nothing"""
+            
+    def shutdown(self, signum, frame):
+        print("Exiting...")
+        sys.exit(0)
+        
+        
+        
+ 
+# Here comes your function definitions
+def main():
+    """Launcher."""    
+    print("Starting HTTProxer server...")
+    # init the GUI or anything else
+    proxy = HTTProxerServer()
+    pass
+ 
+if __name__ == "__main__":
+    main()
